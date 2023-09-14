9f70998c6e4d623c0652ebca78e0f8099cce6200
Sixels <sixels@protonmail.com>
2023-09-14 15:54:19 -0300

    move runtime to rust

---
+++ runtime/src/lib.rs
@@ -0,0 +1,16 @@
+#[no_mangle]
+extern "C" fn __rinha_rt_print_str(data: *const u8, len: usize) {
+    let slice = unsafe { std::slice::from_raw_parts(data, len) };
+    let string = std::str::from_utf8(slice).unwrap();
+    println!("{string}");
+}
+
+#[no_mangle]
+extern "C" fn __rinha_rt_print_int(value: i32) {
+    println!("{value}");
+}
+
+#[no_mangle]
+extern "C" fn __rinha_rt_print_boolean(value: bool) {
+    println!("{value}");
+}
