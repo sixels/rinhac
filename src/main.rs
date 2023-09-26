ace3dd72ab65fb8e5454d47f641e71ed6e9a2be5
Sixels <sixels@protonmail.com>
2023-09-26 17:03:31 -0300

    make CLI comply with the rules

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use rinhac::{ast, compiler::Rinhac};
 
 fn main() {
-    let ast_file_path = std::env::args().nth(1).expect("no file given");
+    let ast_file_path = std::env::args()
+        .nth(1)
+        .unwrap_or(String::from("/var/rinha/source.rinha.json"));
     let ast_file = std::fs::File::open(ast_file_path).expect("failed to open file");
 
     let ast: ast::File = serde_json::from_reader(&ast_file).expect("failed to parse ast file");
